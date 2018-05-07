fd2719de8bb8be5f9127edbdf7fe3de13d4e1fc2
CYY <837023110@qq.com>
2018-05-07 11:46:06 +0800

    初始化模版

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .indx
+    h1 我是首页
+</template>
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  h1
+    color #ff0000
+</style>
+
